bf13af1549b5c43fa86866714d196543e8933289
CHELSEA DOLE <chelseadole@gmail.com>
2017-11-21 17:49:10 -0800

    fixing second test

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -19,32 +19,16 @@ class FactoryUserBoi(factory.django.DjangoModelFactory):
     email = factory.Sequence(lambda user: '{}@codefellows.gov'.format(user))
 
 
-class FactoryProfileBoi(factory.django.DjangoModelFactory):
-    """Factory for creating test Profile."""
-
-    class Meta:
-        """Meta class."""
-
-        model = Profile
-
-    website = "www.yahoo.com"
-    fee = 222.22
-    camera = "Fujifilm"
-    bio = "This is where a bio would be. Happy face."
-    phone = 1876543210
-    user = FactoryUserBoi()
-
-
 class ProfileTests(TestCase):
     """Imager Prof."""
 
     def setUp(self):
         """Generate users using Factory Boiiii."""
-        test_users = [FactoryProfileBoi.create() for i in range(20)]
+        test_users = [FactoryUserBoi.create() for i in range(20)]
 
-        for person in test_users:
-            person.user.set_password('percentsignbois')
-            person.user.save()
+        for user in test_users:
+            user.set_password('percentsignbois')
+            user.save()
 
         self.users = test_users
 
@@ -56,21 +40,30 @@ class ProfileTests(TestCase):
 
     def test_profile_has_attributes_and_is_created(self):
         """Test that a created user has correct attributes."""
-        example = Profile.objects.first()
-        self.assertEqual(example.website, "www.yahoo.com")
-        self.assertEqual(example.user.email, User.objects.first().email)
-
-    def test_user_built_in_adds_new_user(self):
-        """User built in class."""
-        self.test_user = User(email='chelsea@chelsea.com', username='blerg')
-        self.test_profile = Profile(
+        test_user = User.objects.first()
+        example = Profile(
             website="www.chelseadole.com",
             fee="1.00",
             active=True,
             camera="Canon",
             bio="My bio",
+            phone=1069147021,
+            user=test_user
+        )
+        self.assertEqual(example.website, "www.chelseadole.com")
+        self.assertEqual(example.user.email, User.objects.first().email)
+
+    def test_user_built_in_adds_new_user(self):
+        """User built in class."""
+        test_user = User.objects.last()
+        test_profile = Profile(
+            website="www.chelseadole.com",
+            fee="1.00",
+            active=False,
+            camera="Canon",
+            bio="My bio",
             phone=2069147021,
-            user=self.test_user
+            user=test_user
         )
         all_users = User.objects.all()
         length = len(User.objects.all())
