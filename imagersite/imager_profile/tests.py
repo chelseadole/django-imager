670703eb89e2ae9f135654b78824595f8fe3ee3f
CHELSEA DOLE <chelseadole@gmail.com>
2017-11-21 17:16:37 -0800

    adding first few Django tests.

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -4,59 +4,76 @@ from django.test import TestCase
 from django.db import models
 from imager_profile.models import ProfileManager, Profile
 from django.contrib.auth.models import User
+import factory
 
 
-def sample_profile():
-    """A sample profile for use in tests."""
-    test = Profile()
-    test.website = "www.google.com"
-    test.fee = 111.11
-    test.active = True
-    test.camera = "Sony"
-    test.bio = "This is where a bio would be."
-    test.phone = 1234567890
-    test.user = User()
-    return test
+class FactoryUserBoi(factory.django.DjangoModelFactory):
+    """Factory for creating test Users."""
+
+    class Meta:
+        """Meta class."""
+
+        model = User
+
+    username = factory.Sequence(lambda user: '{} McFace'.format(user))
+    email = factory.Sequence(lambda user: '{}@codefellows.gov'.format(user))
 
 
-sample_profile().save()
+class FactoryProfileBoi(factory.django.DjangoModelFactory):
+    """Factory for creating test Profile."""
+
+    class Meta:
+        """Meta class."""
+
+        model = Profile
+
+    website = "www.yahoo.com",
+    fee = 222.22,
+    camera = "Fujifilm",
+    bio = "This is where a bio would be. Happy face.",
+    phone = 9876543210,
+    user = FactoryUserBoi()
 
 
 class ProfileTests(TestCase):
-    """Define the ProfileTests class."""
+    """Imager Prof."""
 
-    def test_profile_has_attributes(self):
-        """Test that the profile class has the correct attributes."""
-        self.assertEqual(sample_profile().website, "www.google.com")
+    def setUp(self):
+        """Generate users using Factory Boiiii."""
+        test_users = [FactoryProfileBoi.create() for i in range(20)]
 
-    def test_new_user_saved_to_db(self):
-        """Test that a new user must be saved to db before it can be used."""
-        test = Profile()
-        test.website = "www.yahoo.com"
-        test.fee = 222.22
-        test.active = True
-        test.camera = "Fujifilm"
-        test.bio = "This is where a bio would be. Happy face."
-        test.phone = 9876543210
-        test.user = User()
-        return test
+        for person in test_users:
+            person.user.set_password('percentsignbois')
+            person.user.save()
+
+        self.users = test_users
+
+    def test_profile_must_have_created_user_class(self):
+        """Test making profile without user."""
+        with self.assertRaises(Exception):
+            this_will_fail = Profile()
+            this_will_fail.save()
+
+    def test_profile_has_attributes_and_is_created(self):
+        """Test that a created user has correct attributes."""
+        example = Profile.objects.first()
+        self.assertEqual(example.website, "www.yahoo.com")
+        self.assertEqual(example.user.email, User.objects.first().email)
 
     def test_user_built_in_adds_new_user(self):
         """User built in class."""
-        chelsea = User(
-            username="dolewhippp",
-            email="chelseadole@gmail.com"
-        )
-        chelsea_profile = Profile(
+        self.test_user = User(email='chelsea@chelsea.com', username='blerg')
+        self.test_profile = Profile(
             website="www.chelseadole.com",
             fee="1.00",
             active=True,
             camera="Canon",
             bio="My bio",
             phone=2069147021,
-            user=self.chelsea
+            user=self.test_user
         )
-        chelsea.save()
         all_users = User.objects.all()
-        self.assertEqual(len(all_users), 1)
-        # self.assertEquals(self.chelsea, chelsea)
+        length = len(User.objects.all())
+        self.assertEqual(len(all_users), length)
+        self.test_user.save()
+        self.assertEqual(len(all_users), length + 1)
