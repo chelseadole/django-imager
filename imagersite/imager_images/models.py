81d995597300b6dd6822ec6c33f73fa7845336b9
CHELSEA DOLE <chelseadole@gmail.com>
2017-11-24 15:46:56 -0800

    adding new tests under Models for Album.

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -15,18 +15,23 @@ class Photo(models.Model):
     """Define the Photo class."""
 
     title = models.CharField(
-        max_length=50)
+        max_length=50,
+        uniqe=True)
     description = models.CharField(
         max_length=200)
     date_uploaded = models.DateField()
     date_modified = models.DateField()
     date_published = models.DateField()
     published = PUBLISH
+    user = models.OneToOneField(
+        User
+    )
 
 
 class Album(models.Model):
     """Define the Album class."""
 
+    photos = []
     title = models.CharField(
         max_length=50, unique=True)
     description = models.CharField(
--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -57,57 +57,66 @@ class AlbumTests(TestCase):
 
     def setUp(self):
         """Generate users using Factory Boiiii."""
-        test_users = [FactoryUserBoi.create() for i in range(20)]
-        test_photos = [FactoryPhotoBoi.create() for i in range(200)]
-        test_albums = [FactoryAlbumBoi.create() for i in range[60]]
-        for user in test_users:
-            user.set_password('percentsignbois')
+        # test_users = [FactoryUserBoi.create() for i in range(20)]
+        # for user in test_users:
+        #     user.set_password('percentsignbois')
+        #     user.save()
+
+        test_photos = [FactoryPhotoBoi.create() for i in range(20)]
+        # test_albums = [FactoryAlbumBoi.create() for i in range[2]]
+        for photo in test_photos:
+            user = FactoryUserBoi.create()
+            photo.user = user
+            # if i % 2 == 0:
+            #     test_albums[i].user = user
+            photo.user.set_password('percentsignbois')
             user.save()
 
-        self.users = test_users
+        # self.users = test_users
         self.photos = test_photos
-        self.albums = test_albums
+        # self.albums = test_albums
 
-    def test_profile_must_have_created_user_class(self):
-        """Test making profile without user."""
-        with self.assertRaises(Exception):
-            this_will_fail = Profile(
-                website="www.chelseadole.com",
-                fee="1.00",
-                camera="Canon",
-                bio="My bio",
-                phone=1069147021,
-                user=User()
-            )
-            this_will_fail.save()
-
-    def test_profile_has_attributes_and_is_created(self):
-        """Test that a created user has correct attributes."""
+    def test_profile_has_correct_title_attribute_when_created(self):
+        """Test that a created user has correct title attribute."""
         test_user = User.objects.first()
-        example = Profile(
-            website="www.chelseadole.com",
-            fee="1.00",
-            camera="Canon",
-            bio="My bio",
-            phone=1069147021,
+        example = Photo(
+            title='mytitle',
+            description='description',
+            date_uploaded=datetime.date(2017, 1, 1),
+            date_modified=datetime.now(),
+            date_published=datetime.date(2017, 11, 1),
+            published='Public',
             user=test_user
         )
-        self.assertEqual(example.website, "www.chelseadole.com")
-        self.assertEqual(example.user.email, User.objects.first().email)
+        self.assertEqual(example.title, "mytitle")
+
+    def test_profile_has_correct_published_attribute_when_initialized(self):
+        """Test that a created user has correct published attribute."""
+        test_user = User.objects.first()
+        example = Photo(
+            title='yeee',
+            description='OWOWOWOWW',
+            date_uploaded=datetime.date(2017, 1, 1),
+            date_modified=datetime.now(),
+            date_published=datetime.date(2017, 10, 1),
+            published='Private',
+            user=test_user
+        )
+        self.assertEqual(example.published, "Private")
 
     def test_user_built_in_adds_new_user(self):
         """User built in class."""
         test_user2 = User.objects.last()
-        example2 = Profile(
-            website="www.chelseadole.com",
-            fee="1.00",
-            camera="Canon",
-            bio="My bio",
-            phone=1069147021,
+        example2 = Photo(
+            title='yeee',
+            description='OWOWOWOWW',
+            date_uploaded=datetime.date(2017, 1, 1),
+            date_modified=datetime.now(),
+            date_published=datetime.date(2017, 10, 1),
+            published='Private',
             user=test_user2
         )
-        self.assertEqual(example2.camera, "Canon")
-        self.assertEqual(example2.user.username, User.objects.last().username)
+        self.assertEqual(str(example2.date_uploaded), '2017-01-01')
 
     def test_new_profile_is_active(self):
         """Ensure that a new profile is active."""
