ed294b681a9ae8a659897f8c3a2fb4667e729f7f
CHELSEA DOLE <chelseadole@gmail.com>
2017-11-28 16:21:40 -0800

    testing continuation.

--- imagersite/imagersite/tests.py
+++ imagersite/imagersite/tests.py
@@ -24,3 +24,22 @@ class ViewTests(TestCase):
         response = self.client.get(reverse_lazy('home'))
         parsed = soup(response.content, 'html.parser')
         self.assertTrue('Welcome, ' not in parsed)
+
+    def test_homeview_retrieves_correct_template(self):
+        """Test that when homeview loads, it loads to base.html."""
+        response = self.client.get(reverse_lazy('home'))
+        self.assertTemplateUsed(response, 'imagersite/base.html')
+
+    def test_login_loads_with_get_request_to_page(self):
+        """Test that when one loads the login page, it works."""
+        response = self.client.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_loads_error_with_incorrect_user(self):
+        """Test that the login view gives error page with wrong user."""
+        login_attempt = {'username': 'thewronglogin', 'password': 'thewrongpassword'}
+        response = self.client.post('/accounts/login/', login_attempt)
+        page_html = soup(response.content, 'html.parser')
+        import pdb; pdb.set_trace()
+        self.assertTemplateUsed(response, 'registration/login.html')
+        self.assertTrue("Please enter a correct username and password. Note that both fields may be case-sensitive." in page_html)
