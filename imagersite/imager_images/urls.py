ee8693e7ec457864032bbea606545804f1f53ee6
CHELSEA DOLE <chelseadole@gmail.com>
2017-11-29 16:41:22 -0800

    added url to imager_images/urls

---
+++ imagersite/imager_images/urls.py
@@ -0,0 +1,8 @@
+"""Urls for images."""
+
+from django.conf.urls import url
+from imager_images.views import LibraryView
+
+urlpatterns = [
+    url(r'^/library$', LibraryView.as_view(), name="library")
+]
