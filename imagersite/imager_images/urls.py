833ff82b5744bbdfd1c9e0ac94f5162fceffae2c
CHELSEA DOLE <chelseadole@gmail.com>
2017-11-30 14:24:01 -0800

    adding beginning of AlbumGalleryView into imager_images.

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -1,8 +1,10 @@
 """Urls for images."""
 
 from django.conf.urls import url
-from imager_images.views import LibraryView
+from imager_images.views import LibraryView, PhotoGalleryView, AlbumGalleryView
 
 urlpatterns = [
-    url(r'^library$', LibraryView.as_view(), name="library")
+    url(r'^library$', LibraryView.as_view(), name="library"),
+    url(r'^photos$', PhotoGalleryView.as_view(), name="photo_gallery"),
+    url(r'^albums$', AlbumGalleryView.as_view(), name="album_gallery")
 ]
--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -12,19 +12,26 @@ class LibraryView(ListView):
     model = Album
     exclude = []
 
-#     def get_context_data(self, user):
-#         """Context data for Library."""
-#         try:
-#             user_object = User.objects.get(username=request)
 
-# try:
-#         user_object = User.objects.get(username=user)
-#         profile_object = user_object.profile
-#         context = {
-#             "user_exists": True,
-#             "user": user_object,
-#             "profile": profile_object
-#         }
-#         return render(request, 'imagersite/altprofile.html', context=context)
-#     except User.DoesNotExist:
-#         return render(request, 'imagersite/altprofile.html', context={"user_exists": False})
+class AlbumGalleryView(ListView):
+    """Gallery of all public albums."""
+    template_name = "imagersite/album_gallery.html"
+    model = Album
+    exclude = []
+
+    def get_context_data(self):
+        """Context data for AlbumGallery."""
+        queryset = Album.objects.filter(published="Public")
+        albums = queryset.all()
+        return {"albums": albums}
+
+
+class PhotoGalleryView(ListView):
+    """Gallery of all public photos."""
+    template_name = "imagersite/photo_gallery.html"
+    model = Photo
+    exclude = []
+
+    def get_context_data(self):
+        """Context data for
+
