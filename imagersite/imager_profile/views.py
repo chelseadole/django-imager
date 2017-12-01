a83596eaa2519d5fcf540a129b552670d1716c7d
CHELSEA DOLE <chelseadole@gmail.com>
2017-11-30 18:28:52 -0800

    starting to add to front-end-3. Changing previous function views into class=based views.

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -4,41 +4,52 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from imager_profile.models import Profile
 from imager_images.models import Photo, Album
-# Create your views here.
+from django.views.generic import DetailView
 
 
-def profile_view(request):
-    """View for profile."""
-    # import pdb; pdb.set_trace()
-    if request.user.username == '':
-        context = {"logged_in": False}
-    else:
-        user_object = User.objects.get(username=request.user.username)
-        prof_object = user_object.profile
-        albums_pub = Album.objects.filter(user=user_object, published="Public").count()
-        albums_pri = Album.objects.filter(user=user_object, published="Private").count()
-        photos = Photo.objects.filter(user=user_object).count()
-        context = {
-            "logged_in": True,
-            "user": user_object,
-            "profile": prof_object,
-            "public_albums": albums_pub,
-            "private_albums": albums_pri,
-            "total_photos": photos
-        }
-    return render(request, 'imagersite/profile.html', context=context)
+class ProfileView(DetailView):
+    """View for user profile page."""
+
+    model = User
+    template_name = "imagersite/profile.html"
+
+    def get_context_data(self, **kwargs):
+        """Context data for user stats."""
+        if not kwargs:
+            context = {"logged_in": False}
+        else:
+            user_object = User.objects.get(username=request.user.username)
+            prof_object = user_object.profile
+            albums_pub = Album.objects.filter(user=user_object, published="Public").count()
+            albums_pri = Album.objects.filter(user=user_object, published="Private").count()
+            photos = Photo.objects.filter(user=user_object).count()
+            context = {
+                "logged_in": True,
+                "user": user_object,
+                "profile": prof_object,
+                "public_albums": albums_pub,
+                "private_albums": albums_pri,
+                "total_photos": photos
+            }
+        return context
 
 
-def alt_profile_view(request, user):
+class AltProfileView(DetailView):
     """View for non-user profile."""
-    try:
-        user_object = User.objects.get(username=user)
-        profile_object = user_object.profile
-        context = {
-            "user_exists": True,
-            "user": user_object,
-            "profile": profile_object
-        }
-        return render(request, 'imagersite/altprofile.html', context=context)
-    except User.DoesNotExist:
-        return render(request, 'imagersite/altprofile.html', context={"user_exists": False})
+
+    model = User
+    template_name = "imagersite/altprofile.html"
+
+    def get_context_data(self, user):
+        """Context data for alt profile."""
+        try:
+            user_object = User.objects.get(username=user)
+            profile_object = user_object.profile
+            context = {
+                "user_exists": True,
+                "user": user_object,
+                "profile": profile_object
+            }
+            return context
+        except User.DoesNotExist:
+            return {"user_exists": False}
