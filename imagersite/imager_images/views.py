01e58045ae644c6cf1d432fb80962ad7c5bb5ad1
CHELSEA DOLE <chelseadole@gmail.com>
2017-12-03 13:32:30 -0800

    final changes to front-end-3.

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -78,7 +78,6 @@ class AddAlbumView(CreateView):
 
     def post(self, request, *args, **kwargs):
         """Post info for new album."""
-        import pdb; pdb.set_trace()
         form_class = self.get_form_class()
         form = self.get_form(form_class)
         if form.is_valid():
